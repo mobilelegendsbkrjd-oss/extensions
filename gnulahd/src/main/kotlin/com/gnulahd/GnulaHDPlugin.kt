2ba0e622486a1d099d364a90c6e8e9d2b81aa54b
mobilelegendsbkrjd-oss <mobilelegendsbkrjd@gmail.com>
2026-04-10 18:52:18 -0600

    Update GnulaHDPlugin.kt

--- gnulahd/src/main/kotlin/com/gnulahd/GnulaHDPlugin.kt
+++ gnulahd/src/main/kotlin/com/gnulahd/GnulaHDPlugin.kt
@@ -4,10 +4,10 @@ import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
 @CloudstreamPlugin
-class GnulahdPlugin: BasePlugin() {
+class GnulaHDPlugin: BasePlugin() {
     override fun load() {
         // All providers should be added in this manner. Please don't edit the providers list directly.
-        registerMainAPI(Gnulahd())
+        registerMainAPI(GnulaHD())
         //registerExtractorAPI(Extractor())
     }
 }
